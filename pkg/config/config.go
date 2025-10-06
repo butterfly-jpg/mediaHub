980333e7911b46aa620e58b8aa54b809936a4fd2
chengzhikun <1746502977@qq.com>
2025-10-07 04:53:09 +0800

    初始化
    初始化
    
    czk20251007

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,62 @@
 package config
+
+import (
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server struct {
+		IP          string `mapstructure:"ip"`
+		Port        int    `mapstructure:"port"`
+		AccessToken string
+	} `mapstructure:"server"`
+	Redis struct {
+		Host string
+		Port int
+		Pwd  string `mapstructure:"pwd"`
+	}
+	Mysql struct {
+		DSN         string `mapstructure:"dsn"`
+		MaxLifeTime int
+		MaxOpenConn int
+		MaxIdleConn int
+	}
+	Log struct {
+		Level   string
+		LogPath string `mapstructure:"logPath"`
+	} `mapstructure:"log"`
+	ShortDomain     string
+	UserShortDomain string
+}
+
+var conf *Config
+
+func InitConfig(filePath string, typ ...string) {
+	v := viper.New()
+	v.SetConfigFile(filePath)
+	if len(typ) > 0 {
+		v.SetConfigType(typ[0])
+	}
+	err := v.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	conf = &Config{}
+	err = v.Unmarshal(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 配置热更新
+	v.OnConfigChange(func(in fsnotify.Event) {
+		v.Unmarshal(conf)
+	})
+	v.WatchConfig()
+}
+
+func GetConfig() *Config {
+	return conf
+}
--- pkg/db/mysql/mysql.go
+++ pkg/db/mysql/mysql.go
@@ -1,1 +1,27 @@
 package mysql
+
+import (
+	"database/sql"
+	"mediahub/pkg/config"
+	"time"
+)
+
+var db *sql.DB
+
+func InitMysql(cnf *config.Config) {
+	var err error
+	if cnf.Mysql.DSN == "" {
+		panic("数据库连接字符串不能为空")
+	}
+	db, err = sql.Open("mysql", cnf.Mysql.DSN)
+	if err != nil {
+		panic(err)
+	}
+	db.SetConnMaxLifetime(time.Duration(cnf.Mysql.MaxLifeTime) * time.Second)
+	db.SetMaxOpenConns(cnf.Mysql.MaxOpenConn)
+	db.SetMaxIdleConns(cnf.Mysql.MaxIdleConn)
+}
+
+func GetDB() *sql.DB {
+	return db
+}
